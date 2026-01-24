b6acb014e427b130dea14f4fa4d7ac2e8be5b52a
itssonya045 <shrirakshe2004@email.com>
2026-01-24 19:23:12 +0530

    deleted movies

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -18,6 +18,42 @@ const createMovie = async (req, res) => {
   }
 };
 
+const deleteMovie = async (req, res) => {
+  try {
+    const response = await Movie.deleteOne({
+      _id: req.params.movieId
+    });
+
+    if (response.deletedCount === 0) {
+      return res.status(404).json({
+        success: false,
+        error: {},
+        message: "Movie not found",
+        data: {}
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      error: {},
+      message: "Successfully deleted the movie",
+      data: response
+    });
+
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      error: error,
+      message: "Something went wrong",
+      data: {}
+    });
+  }
+};
+
+
+
 module.exports = {
-  createMovie
+  createMovie,
+  deleteMovie
 };
--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -1,7 +1,8 @@
 const MovieController =  require("../controllers/movie.controller")
 
 const routes = (app)=>{
-    app.post("/mba/api/v1/movies",MovieController.createMovie)
+    app.post("/mba/api/v1/movies",MovieController.createMovie),
+    app.delete("/mba/api/v1/movies/:movieId",MovieController.deleteMovie)
 }
 
 module.exports = routes
